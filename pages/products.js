7e8ab54f77ebc91a3290b8e7135ff20752863687
faizalkhan <faizalkhan.s@gmail.com>
2025-12-22 13:11:20 +0530

    product page in 5 columns

--- pages/products.js
+++ pages/products.js
@@ -106,7 +106,7 @@ export default function Products({ products }) {
         </div>
 
         {/* Products Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-8">
           {filteredProducts.map((product) => (
             <div key={product._id}>
               <ProductCard product={product} />
